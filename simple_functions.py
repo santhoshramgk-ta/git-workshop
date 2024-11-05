079ac8d840eae395d960b11c5f5b10c25ac75bdd
sathishchandra-ta <uppalapati.vsms@tigeranalytics.com>
2024-11-05 12:16:13 +0530

    Added docstrings to functions

--- simple_functions.py
+++ simple_functions.py
@@ -1,7 +1,35 @@
 # Custom python functions
 
 def double_number(a):
+    """
+     Doubles the value of the input number and prints the value before and after doubling.
+
+    Parameters:
+    a (int or float): The number to be doubled.
+
+    Returns:
+    None: This function prints the result but does not return any value.
+    
+    Example:
+    >>> double_number(4)
+    value before double_number(): 4
+    value after double_number(): 8
+    """
     return a+a
 
 def square_number(a):
+    """
+    Squares the value of the input number and prints the value before and after squaring.
+
+    Parameters:
+    a (int or float): The number to be squared.
+
+    Returns:
+    None: This function prints the result but does not return any value.
+
+    Example:
+    >>> square_number(4)
+    value before square_number(): 4
+    value after square_number(): 16
+    """
     return a*a
